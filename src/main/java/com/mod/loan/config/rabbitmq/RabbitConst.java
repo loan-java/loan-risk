964a378d614aee8f0ec714bd99235a0e960f4e06
shuailj <178397218@qq.com>
2019-05-05 21:57:35 +0800

    添加快钱支付queue

--- src/main/java/com/mod/loan/config/rabbitmq/RabbitConst.java
+++ src/main/java/com/mod/loan/config/rabbitmq/RabbitConst.java
@@ -2,11 +2,15 @@ package com.mod.loan.config.rabbitmq;
 
 public class RabbitConst {
 
+    public final static String baofoo_queue_order_pay = "baofoo_queue_order_pay"; // 宝付放款队列
+
+
+    public final static String kuaiqian_queue_order_pay = "kuaiqian_queue_order_pay"; // 快钱放款队列
+
 
     public final static String qjld_queue_risk_order_notify = "qjld_queue_risk_order_notify"; // 全景雷达风控订单审核通知
     public final static String qjld_queue_risk_order_query = "qjld_queue_risk_order_result"; // 全景雷达风控订单结果查询
 
-    public final static String baofoo_queue_order_pay = "baofoo_queue_order_pay"; // 放款队列
 
     public final static String qjld_queue_risk_order_query_wait = "qjld_queue_risk_order_result_wait"; // 全景雷达风控订单结果等待10s
 
