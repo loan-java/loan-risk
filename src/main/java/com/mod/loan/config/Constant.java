14b96e28c6a11809a92c27785816a6d25a600293
shuailj <178397218@qq.com>
2019-05-07 20:49:43 +0800

    配置修改

--- src/main/java/com/mod/loan/config/Constant.java
+++ src/main/java/com/mod/loan/config/Constant.java
@@ -9,9 +9,6 @@ public class Constant {
 	public static String TEST;
 
 	public static String ENVIROMENT;
-	public static String SERVER_API_URL;
-	public static String SERVER_H5_URL;
-	public static String SERVER_ITF_URL;
 
 	@Value("${test:}")
 	public void setPICTURE_URL(String test) {
@@ -23,19 +20,6 @@ public class Constant {
 		Constant.ENVIROMENT = environment;
 	}
 
-	@Value("${server.api.url:}")
-	public void setServerApiUrl(String serverApiUrl) {
-		SERVER_API_URL = serverApiUrl;
-	}
 
-	@Value("${server.h5.url:}")
-	public void setServerH5Url(String serverH5Url) {
-		SERVER_H5_URL = serverH5Url;
-	}
-
-	@Value("${server.itf.url:}")
-	public void setServerItfUrl(String serverItfUrl) {
-		SERVER_ITF_URL = serverItfUrl;
-	}
 
 }
