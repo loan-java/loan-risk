bcbe4300d2f76a8d03f157746b2114f3eeb1db98
shuailj <178397218@qq.com>
2019-05-27 15:23:38 +0800

    银行卡修改

--- src/main/java/com/mod/loan/service/impl/QjldPolicyServiceImpl.java
+++ src/main/java/com/mod/loan/service/impl/QjldPolicyServiceImpl.java
@@ -96,7 +96,9 @@ public class QjldPolicyServiceImpl implements QjldPolicyService {
         userDTO.setId_name(user.getUserName());
         userDTO.setId_card(user.getUserCertNo());
         userDTO.setPhone(user.getUserPhone());
-        //userDTO.setBank_card_no(userBank.getCardNo());
+        if (userBank != null && userBank.getCardNo() != null) {
+            userDTO.setBank_card_no(userBank.getCardNo());
+        }
         reqDTO.setBase_user(userDTO);
         //添加商户信息(新颜分配的商户号)
         reqDTO.setMember_id(qjldConfig.getQjldMemberId());
