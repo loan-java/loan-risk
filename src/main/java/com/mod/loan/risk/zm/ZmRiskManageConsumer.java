a824195dbb252f82951725f1bdca62356c6a3c68
weihanwen <whw123456>
2019-07-02 13:26:40 +0800

    代码位置调整

--- src/main/java/com/mod/loan/risk/zm/ZmRiskManageConsumer.java
+++ src/main/java/com/mod/loan/risk/zm/ZmRiskManageConsumer.java
@@ -55,55 +55,55 @@ public class ZmRiskManageConsumer {
     @RabbitHandler
     public void risk_order_notify(Message mess) {
         RiskAuditMessage riskAuditMessage = JSONObject.parseObject(mess.getBody(), RiskAuditMessage.class);
-        log.info("指迷风控信息,[notify]：" + riskAuditMessage.toString());
         Order order = null;
         OrderUser orderUser = null;
         Long uid = null;
         String orderNo = null;
-        //聚合订单校验
-        if (riskAuditMessage.getSource() == ConstantUtils.ZERO && riskAuditMessage.getOrderId() != null) {
-            order = orderService.selectByPrimaryKey(riskAuditMessage.getOrderId());
-            if (!redisMapper.lock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderId(), 30)) {
-                log.error("指迷风控消息重复，message={}", JSON.toJSONString(riskAuditMessage));
-                return;
-            }
-            if (order == null || order.getUid() == null || order.getOrderNo() == null) {
-                log.info("指迷风控，订单不存在 message={}", JSON.toJSONString(riskAuditMessage));
-                return;
-            }
-            if (order.getStatus() != 11) { // 新建的订单才能进入风控模块
-                log.info("指迷风控，无效的订单状态 message={}", JSON.toJSONString(riskAuditMessage));
-                return;
-            }
-            uid = order.getUid();
-            orderNo = order.getOrderNo();
-            //融泽订单校验
-        } else if (riskAuditMessage.getSource() == ConstantUtils.ONE && riskAuditMessage.getOrderNo() != null) {
-            orderUser = orderUserService.selectByOrderNo(riskAuditMessage.getOrderNo());
-            if (!redisMapper.lock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderNo(), 30)) {
-                log.error("指迷风控消息重复，message={}", JSON.toJSONString(riskAuditMessage));
-                return;
-            }
-            if (orderUser == null || orderUser.getUid() == null || orderUser.getOrderNo() == null) {
-                log.info("指迷风控，订单不存在 message={}", JSON.toJSONString(riskAuditMessage));
-                return;
-            }
-            uid = orderUser.getUid();
-            orderNo = orderUser.getOrderNo();
-        } else {
-            log.error("指迷风控消息错误，message={}", JSON.toJSONString(riskAuditMessage));
-            return;
-        }
-        Merchant merchant = merchantService.findMerchantByAlias(riskAuditMessage.getMerchant());
-        if (merchant == null) {
-            log.info("指迷风控，无效的商户 message={}", riskAuditMessage.getMerchant());
-            return;
-        }
-        if (!ConstantUtils.THREE.equals(merchant.getRiskType())) {
-            log.info("指迷风控，无效的风控类型 message={}", riskAuditMessage.getMerchant());
-            return;
-        }
         try {
+            log.info("指迷风控信息,[notify]：" + JSON.toJSONString(riskAuditMessage));
+            //聚合订单校验
+            if (riskAuditMessage.getSource() == ConstantUtils.ZERO && riskAuditMessage.getOrderId() != null) {
+                order = orderService.selectByPrimaryKey(riskAuditMessage.getOrderId());
+                if (!redisMapper.lock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderId(), 30)) {
+                    log.error("指迷风控消息重复，message={}", JSON.toJSONString(riskAuditMessage));
+                    return;
+                }
+                if (order == null || order.getUid() == null || order.getOrderNo() == null) {
+                    log.info("指迷风控，订单不存在 message={}", JSON.toJSONString(riskAuditMessage));
+                    return;
+                }
+                if (order.getStatus() != 11) { // 新建的订单才能进入风控模块
+                    log.info("指迷风控，无效的订单状态 message={}", JSON.toJSONString(riskAuditMessage));
+                    return;
+                }
+                uid = order.getUid();
+                orderNo = order.getOrderNo();
+                //融泽订单校验
+            } else if (riskAuditMessage.getSource() == ConstantUtils.ONE && riskAuditMessage.getOrderNo() != null) {
+                orderUser = orderUserService.selectByOrderNo(riskAuditMessage.getOrderNo());
+                if (!redisMapper.lock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderNo(), 30)) {
+                    log.error("指迷风控消息重复，message={}", JSON.toJSONString(riskAuditMessage));
+                    return;
+                }
+                if (orderUser == null || orderUser.getUid() == null || orderUser.getOrderNo() == null) {
+                    log.info("指迷风控，订单不存在 message={}", JSON.toJSONString(riskAuditMessage));
+                    return;
+                }
+                uid = orderUser.getUid();
+                orderNo = orderUser.getOrderNo();
+            } else {
+                log.error("指迷风控消息错误，message={}", JSON.toJSONString(riskAuditMessage));
+                return;
+            }
+            Merchant merchant = merchantService.findMerchantByAlias(riskAuditMessage.getMerchant());
+            if (merchant == null) {
+                log.info("指迷风控，无效的商户 message={}", riskAuditMessage.getMerchant());
+                return;
+            }
+            if (!ConstantUtils.THREE.equals(merchant.getRiskType())) {
+                log.info("指迷风控，无效的风控类型 message={}", riskAuditMessage.getMerchant());
+                return;
+            }
             User user = userService.selectByPrimaryKey(uid);
             //开始请求2.2接口
             DecisionZmDetail zmDetail = zmDetailService.creditApply(user, orderNo);
@@ -149,31 +149,32 @@ public class ZmRiskManageConsumer {
             //风控异常重新提交订单或者进入人工审核
             log.error("指迷风控异常{}", JSON.toJSONString(riskAuditMessage));
             log.error("指迷风控异常", e);
+            if (riskAuditMessage.getTimes() < 6) {
+                riskAuditMessage.setTimes(riskAuditMessage.getTimes() + 1);
+                rabbitTemplate.convertAndSend(RabbitConst.zm_queue_risk_order_notify, riskAuditMessage);
+            }else{
+                if (riskAuditMessage.getSource() == ConstantUtils.ZERO) {
+                    //聚合风控下单异常直接转入人工审核
+                    order.setStatus(ConstantUtils.unsettledOrderStatus);
+                    orderService.updateOrderByRisk(order);
+                } else if (riskAuditMessage.getSource() == ConstantUtils.ONE) {
+                    //融泽风控查询异常直接返回审批失败
+                    DecisionZmDetail zmDetail = new DecisionZmDetail();
+                    zmDetail.setReturnCode("-1");
+                    zmDetail.setReturnInfo("fail");
+                    zmDetail.setScore("0.0");
+                    zmDetail.setOrderNo(riskAuditMessage.getOrderNo());
+                    zmDetail.setCreatetime(new Date());
+                    zmDetail.setUpdatetime(new Date());
+                    zmDetailService.insert(zmDetail);
+                    callbackThird(orderUser, zmDetail);
+                }
+            }
+        }finally {
             if (riskAuditMessage.getSource() == ConstantUtils.ZERO) {
                 redisMapper.unlock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderId());
             } else if (riskAuditMessage.getSource() == ConstantUtils.ONE) {
                 redisMapper.unlock(RedisConst.ORDER_POLICY_LOCK + riskAuditMessage.getOrderNo());
-            }
-            if (riskAuditMessage.getTimes() < 6) {
-                riskAuditMessage.setTimes(riskAuditMessage.getTimes() + 1);
-                rabbitTemplate.convertAndSend(RabbitConst.zm_queue_risk_order_notify, riskAuditMessage);
-                return;
-            }
-            if (riskAuditMessage.getSource() == ConstantUtils.ZERO) {
-                //聚合风控下单异常直接转入人工审核
-                order.setStatus(ConstantUtils.unsettledOrderStatus);
-                orderService.updateOrderByRisk(order);
-            } else if (riskAuditMessage.getSource() == ConstantUtils.ONE) {
-                //融泽风控查询异常直接返回审批失败
-                DecisionZmDetail zmDetail = new DecisionZmDetail();
-                zmDetail.setReturnCode("-1");
-                zmDetail.setReturnInfo("fail");
-                zmDetail.setScore("0.0");
-                zmDetail.setOrderNo(riskAuditMessage.getOrderNo());
-                zmDetail.setCreatetime(new Date());
-                zmDetail.setUpdatetime(new Date());
-                zmDetailService.insert(zmDetail);
-                callbackThird(orderUser, zmDetail);
             }
         }
     }
