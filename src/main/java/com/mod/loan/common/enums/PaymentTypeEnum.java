7f3c2521f997a5f84936bef7272b3e9f57877008
shuailj <178397218@qq.com>
2019-10-27 15:42:52 +0800

    支付字段修改

--- src/main/java/com/mod/loan/common/enums/PaymentTypeEnum.java
+++ src/main/java/com/mod/loan/common/enums/PaymentTypeEnum.java
@@ -1,30 +1,70 @@
 package com.mod.loan.common.enums;
 
-/**
- * 第三方放款得支付类型
- * @author kk
- */
 public enum PaymentTypeEnum {
-    /**
-     * 支付类型
-     */
-    BAOFOO("baofoo", "宝付"),
-    KUAIQIAN("kuaiqian", "快钱");
+    baofoo(4, "baofoo", "宝付"),
+    kuaiqian(5, "kuaiqian", "快钱"),
+    chanpay(6, "chanpay", "畅捷"),
+    yeepay(7, "yeepay", "易宝"),
+    ;
 
-    private String code;
-    private String msg;
+    private Integer code;
+    private String name;
+    private String desc;
 
-    PaymentTypeEnum(String code, String msg) {
+    PaymentTypeEnum(Integer code, String name, String desc) {
         this.code = code;
-        this.msg = msg;
+        this.name = name;
+        this.desc = desc;
     }
 
+    public static String getDescByName(String name) {
+        for (PaymentTypeEnum e : PaymentTypeEnum.values()) {
+            if (e.getName().equals(name)) {
+                return e.getDesc();
+            }
+        }
+        return "";
+    }
 
-    public String getCode() {
+    public static String getDesc(Integer code) {
+        for (PaymentTypeEnum status : PaymentTypeEnum.values()) {
+            if (status.getCode().equals(code)) {
+                return status.getDesc();
+            }
+        }
+        return null;
+    }
+
+    public static String getName(Integer code) {
+        for (PaymentTypeEnum status : PaymentTypeEnum.values()) {
+            if (status.getCode().equals(code)) {
+                return status.getName();
+            }
+        }
+        return null;
+    }
+
+    public Integer getCode() {
         return code;
     }
 
-    public String getMsg() {
-        return msg;
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
